65677b699bbad3bcc85804d2340a135982dd711b
Kovalenko Igor <i.kovalenko@cova-macbook.local>
2020-12-02 07:09:43 +0300

    Athentificator 1.0

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using OtpNet;
+
+namespace Authentificator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chars1 = "hyh5gt5c5cncvikscx5vlftecoeygy7d";
+            var bytes = Base32Encoding.ToBytes(chars1);
+
+            var totp = new Totp(bytes);
+
+            var result = totp.ComputeTotp();
+            var remainingTime = totp.RemainingSeconds();
+            Console.WriteLine(result);
+        }
+    }
+}
